e3575e110644586b81a97fe4bbe14c7c819e499a
unknown <puneet@LAPTOP-I16BOK9Q>
2018-03-30 16:03:52 +0530

    adding setup.py

---
+++ spark_etl/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SparkETL',
+    version='0.1',
+    packages=['', 'Extract'],
+    package_dir={'': 'spark_etl'},
+    url='',
+    license='',
+    author='Puneet Tripathi',
+    author_email='puneet.tripathim@gmail.com',
+    description='ETL on Apache Spark in python'
+)
